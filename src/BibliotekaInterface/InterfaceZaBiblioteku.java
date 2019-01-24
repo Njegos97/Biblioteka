163bf97bbec082cbebe8fea37ae881e11513725a
njegoss997@gmail.com <njegoss997@gmail.com>
2019-01-24 15:27:31 +0100

    Implementovao interface sa metodama: public int generisiKey(Map<Integer, Object> lista); public boolean izbrisi(Map<Integer, Object> lista); public Object pretraziListu(Map<Integer, Object> lista);
    I Implemenotavo metodu Registracija u Klasi BibliotekaLogika u Paketu logika

---
+++ src/BibliotekaInterface/InterfaceZaBiblioteku.java
@@ -0,0 +1,12 @@
+package BibliotekaInterface;
+
+
+import java.util.*;
+
+public interface InterfaceZaBiblioteku  {
+
+	public int generisiKey(Map<Integer, Object> lista);
+		
+	
+	
+}
---
+++ src/logika/BibliotekaLogika.java
@@ -0,0 +1,12 @@
+package logika;
+import java.util.*;
+import entiteti.Clan;
+public class BibliotekaLogika {
+
+	
+	public static boolean registracija(LinkedHashMap<Integer, Clan> Clan) {
+		
+		
+	}
+
+}

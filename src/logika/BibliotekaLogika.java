5acc444255219811fcd1b50d05e5e10f9d85f7c1
njegoss997@gmail.com <njegoss997@gmail.com>
2019-01-25 10:47:36 +0100

    Prepravio metodu Registracija u Klasi BibliotekaLogika u paketu logika da generisani key dodjeli Clanu u kolekciji LinkedHashMap

--- src/logika/BibliotekaLogika.java
+++ src/logika/BibliotekaLogika.java
@@ -1,18 +1,38 @@
 package logika;
 import java.util.*;
+import BibliotekaInterface.InterfaceZaBiblioteku;
 import entiteti.Clan;
-public class BibliotekaLogika {
+public class BibliotekaLogika implements InterfaceZaBiblioteku {
 
 	
 	public static boolean registracija(LinkedHashMap<Integer, Clan> listaClanova, Clan clanKojiSeRegistruje) {
 		
-		    
-			Integer key = 0;
-			return listaClanova.put(key, clanKojiSeRegistruje);
-			
-			
-		
+		    BibliotekaLogika.
+			Integer key =0 ;
+		    listaClanova.put(key, clanKojiSeRegistruje);
+		   
+		    return true;
+			}
+
+	@Override
+	public int generisiKey(Map<Integer, Object> lista) {
+		int a = 0;
+		return 0;
 	}
+
+	@Override
+	public boolean izbrisi(Map<Integer, Object> lista) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Object pretraziListu(Map<Integer, Object> lista) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
 	
 
 }

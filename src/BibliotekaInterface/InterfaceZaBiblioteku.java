3e89afe46ef6715fab2848e85d13fd86fe238094
njegoss997@gmail.com <njegoss997@gmail.com>
2019-01-25 09:13:52 +0100

    kreirao metode u interfaceu za biblioteku

--- src/BibliotekaInterface/InterfaceZaBiblioteku.java
+++ src/BibliotekaInterface/InterfaceZaBiblioteku.java
@@ -6,6 +6,12 @@ import java.util.*;
 public interface InterfaceZaBiblioteku  {
 
 	public int generisiKey(Map<Integer, Object> lista);
+	
+	public boolean izbrisi(Map<Integer, Object> lista);
+	
+	public Object pretraziListu(Map<Integer, Object> lista);
+
+	
 		
 	
 	
